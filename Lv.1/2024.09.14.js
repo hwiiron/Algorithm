8831da5cd22de21b44c2fef2ef16ae38c631967a
Hwichul Shin <shinhwiiron@gmail.com>
2024-09-14 15:25:03 +0900

    2024.09.14.js Lv.1

---
+++ Lv.1/2024.09.14.js
@@ -0,0 +1,14 @@
+// 짝수와 홀수
+
+// 정수 num이 짝수일 경우 "Even"을 반환하고 홀수인 경우 "Odd"를 반환하는 함수, solution을 완성해주세요.
+
+function solution(num) {
+  var answer = '';
+  
+  // num을 2로 나눴을 때 몫이 1이면 "Odd", 몫이 0이면 "Even"
+  // 삼항 연산자에서 몫이 1이면 true로 왼쪽 값이 출력되고, 0이면 false로 오른쪽 값이 출력된다.
+  return answer = num % 2 ? "Odd" : "Even";
+}
+
+console.log(solution(3)); // Odd
+console.log(solution(4)); // Even
