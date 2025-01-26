5f402c1c53d07f8a67e002346400609ad8ceb56b
HwichulShin <shinhwiiron@gmail.com>
2025-01-26 10:46:14 +0900

    2025.01.26 Lv.0

---
+++ Lv.0/2025.01.26.js
@@ -0,0 +1,27 @@
+// 수열과 구간 쿼리 1
+
+// 정수 배열 arr와 2차원 정수 배열 queries이 주어집니다.
+// queries의 원소는 각각 하나의 query를 나타내며, [s, e] 꼴입니다.
+// 각 query마다 순서대로 s ≤ i ≤ e인 모든 i에 대해 arr[i]에 1을 더합니다.
+// 위 규칙에 따라 queries를 처리한 이후의 arr를 return 하는 solution 함수를 완성해 주세요.
+
+function solution(arr, queries) {
+  for (let i = 0; i < queries.length; i++) {
+    for (let j = queries[i][0]; j <= queries[i][1]; j++) {
+      arr[j] += 1; // 구간에 해당하는 원소를 1씩 증가
+    }
+  }
+
+  return arr;
+}
+
+console.log(
+  solution(
+    [0, 1, 2, 3, 4],
+    [
+      [0, 1],
+      [1, 2],
+      [2, 3],
+    ]
+  )
+); // [1, 3, 4, 4, 4]
